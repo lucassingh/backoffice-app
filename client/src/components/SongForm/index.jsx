c709acf7b624c88c06ad12ff440120354bfa9064
lucas.singh <lucas.singh@bizitglobal.com>
2022-10-15 20:27:51 -0300

    validacion de formulario y alertas

--- client/src/components/SongForm/index.jsx
+++ client/src/components/SongForm/index.jsx
@@ -2,10 +2,27 @@ import { useState } from "react";
 import axios from 'axios';
 import FileInput from "../FileInput";
 import styles from "./styles.module.css";
+import { useNavigate } from "react-router-dom";
 
 const SongForm = () => {
+
+    const navigate = useNavigate();
+
+    const initialValue = {
+        name: "",
+        category: "",
+        artist: "",
+        song: "",
+        img: "",
+    }
+
+    const [error, setError] = useState(false);
+
+    const [display, setDisplay] = useState(false);
+
     const [data, setData] = useState({
         name: "",
+        category: "",
         artist: "",
         song: "",
         img: "",
@@ -20,57 +37,97 @@ const SongForm = () => {
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
+
+        if (data.name === '' || data.category === '' || data.artist === '' || data.song === '' || data.img === '') {
+            setError(true)
+            return;
+        }
+
         try {
             const url = process.env.REACT_APP_API_URL + "/songs"
-            const { data: res } = await axios.post(url, data);
-            console.log(res)
+            await axios.post(url, data);
+            setData(initialValue);
+            setDisplay(true);
+            setTimeout(() => {                
+                navigate('/')
+            }, 3000);
+
         } catch (error) {
             console.log(error)
         }
     };
 
     return (
-        <div className={styles.containerForm}>
-            <form className={styles.form} onSubmit={handleSubmit} >
-                <input
-                    type="text"
-                    className={styles.input}
-                    placeholder="Nombre archivo"
-                    name="name"
-                    onChange={handleChange}
-                    value={data.name}
-                />
-                <input
-                    type="text"
-                    className={styles.input}
-                    placeholder="Autor"
-                    name="artist"
-                    onChange={handleChange}
-                    value={data.artist}
-                />
-                <FileInput
-                    name="img"
-                    label="Elegir imagen para podcast"
-                    handleInputState={handleInputState}
-                    type="image"
-                    value={data.img}
-                />
-                <FileInput
-                    name="song"
-                    label="Elegir archivo"
-                    handleInputState={handleInputState}
-                    type="audio"
-                    value={data.song}
-                />
-                <div className={styles.contButton}>
-                    <button type="submit" className='btn btn-primary' >
-                        Cargar
-                    </button>
-                </div>
+        <>
+            {
+                display && (
+                    <div className="alert alert-success" role="alert">
+                        Cargado correctamente!!!
+                    </div>
+                )
+            }
 
-            </form>
-        </div>
+            <div className={styles.containerForm}>
+                <form className={styles.form} onSubmit={handleSubmit} noValidate>
+                    <input
+                        type="text"
+                        className={styles.input}
+                        placeholder="Nombre podcast"
+                        name="name"
+                        onChange={handleChange}
+                        value={data.name}
+                    />
+                    {error && data.name.length <= 0 ? <span className={styles.validation}>El nombre del podcast es requerido</span> : ''}
+
+
+                    <input
+                        type="text"
+                        className={styles.input}
+                        placeholder="Categoría"
+                        name="category"
+                        onChange={handleChange}
+                        value={data.category}
+                    />
+                    {error && data.category.length <= 0 ? <span className={styles.validation}>La categoría es requerida</span> : ''}
+
+                    <input
+                        type="text"
+                        className={styles.input}
+                        placeholder="Autor"
+                        name="artist"
+                        onChange={handleChange}
+                        value={data.artist}
+                    />
+                    {error && data.artist.length <= 0 ? <span className={styles.validation}>El autor es requerido</span> : ''}
+
+                    <FileInput
+                        name="img"
+                        label="Imagen Podcast"
+                        handleInputState={handleInputState}
+                        type="image"
+                        value={data.img}
+                    />
+                    {error && data.img.length <= 0 ? <span className={styles.validation}>La imagen del podcast es requerida</span> : ''}
+
+                    <FileInput
+                        name="song"
+                        label="Archivo mp3"
+                        handleInputState={handleInputState}
+                        type="audio"
+                        value={data.song}
+                    />
+                    {error && data.song.length <= 0 ? <span className={styles.validation}>la imagen del podcast es requerida</span> : ''}
+
+                    <div className={styles.contButton}>
+                        <button type="submit" className='btn btn-primary' >
+                            Cargar
+                        </button>
+                    </div>
+
+                </form>
+            </div>
+        </>
     );
 };
 
--- client/src/pages/FilesScreen.jsx
+++ client/src/pages/FilesScreen.jsx
@@ -4,9 +4,8 @@ import SongForm from '../components/SongForm';
 export const FilesScreen = () => {
     return (
         <div>
-            <Jumbotron title='Cargar archivos PDF' subtitle='Completar el formulario para cargar archivos en la base de datos'/>
-
-            <div className="container d-flex justify-content-center">
+            <Jumbotron title='Cargar Archivos PDF' subtitle='Completar el formulario para cargar archivos PDF en la base de datos' />
+            <div className="container d-flex justify-content-center flex-column custom-container">
                 <SongForm />
             </div>
         </div>
